c18b16cf2e6c4747c11b22126d3d9266a530d536
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-14 17:20:39 -0300

    feat: Começo da funcionalidade de carrosel

--- script.js
+++ script.js
@@ -19,3 +19,41 @@ window.onresize = () => {
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
 }
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual < (numeroSlides - 1)) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarSlideAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+        slideAtual = numeroSlides - 1
+    }
+
+    banner.classList.add(slides[slideAtual])
+}
